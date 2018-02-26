c466cb587bb186dd9b32b029f52d44d1b52d05a8
avastermark19 <30867791+avastermark19@users.noreply.github.com>
2018-02-26 23:30:58 +0100

    Add files via upload

---
+++ install.pl
@@ -0,0 +1,20 @@
+#! /usr/bin/perl -w
+use strict;
+
+my $internet = (split(' ', ` ping -c 1 goole.com `))[0];
+if($internet eq 'PING') { print 'Internet OK', "\n"; } else { print 'Check your internet connection', "\n"; exit; }
+
+` cp ~/mfs/Pfam-A* . `;
+` cp ~/mfs/uniprot* . `;
+` cp ~/mfs/topcons* . `;
+` cp ~/mfs/TMalign* . `;
+
+` chmod u+x *.pl `;
+` chmod u+x *.exe `;
+` chmod u+x *.b `;
+` chmod u+x *.bb `;
+` chmod u+x *.bbb `;
+` chmod u+x TMalign `;
+
+exit;
+
