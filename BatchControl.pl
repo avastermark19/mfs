c74bc3720e133bda8e4152ed686e9fc3d24804ab
avastermark19 <30867791+avastermark19@users.noreply.github.com>
2018-03-15 21:12:42 +0100

    Add files via upload

---
+++ BatchControl.pl
@@ -0,0 +1,40 @@
+#! /usr/bin/perl -w
+use strict;
+ use Term::ANSIColor qw(:constants);
+
+system("clear");
+
+` rm -rf BatchControl `;
+` mkdir BatchControl `;
+
+my @list = ` ls -d BATCH_* `;
+#print BLUE, scalar @list, RESET, "\n";
+
+for(my $i=0; $i<@list; $i++) {
+chomp $list[$i];
+
+system("clear");
+print BLUE, $i, '/', (scalar @list), RESET, "\n";
+print RED, $list[$i], RESET, "\n";
+my @line = split('_', $list[$i]);
+#print $line[1], "\n";
+#print $line[2], "\n";
+
+` rm -rf BACKUP `;
+` mkdir BACKUP `;
+` cp -r $list[$i]/* BACKUP/ `;
+
+system(" ./GLOBAL_run.pl $line[1] $line[2] 4 5 3 2 ");
+
+` mkdir BatchControl/$list[$i] `;
+` mv step8.out BatchControl/$list[$i] `;
+` mv step14.out BatchControl/$list[$i] `;
+
+
+ 
+}
+
+
+exit;
+
+
