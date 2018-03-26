d698e30a9b6adacd4acd54545b021f14fec222e6
avastermark19 <30867791+avastermark19@users.noreply.github.com>
2018-03-26 10:10:05 +0200

    Add files via upload

--- step19.pl
+++ step19.pl
@@ -3,6 +3,49 @@ use strict;
 
  use Term::ANSIColor qw(:constants);
 
+my $LOOP_LENGTH=30;
+#####################################################################
+# http://ecoli.med.utoronto.ca/membrane/php/home.php
+# https://www.uniprot.org/docs/ecoli.txt
+# http://ecoli.med.utoronto.ca/membrane/php/data/Babu_et_al_2017_Ecoli_CEP_PPI_APMS_data.zip
+# aake@ae17a:~/clone30/mfs$ blastall -p blastp -i SV2A.substr -d uniprot_sprot.fasta -m 8 -e 1000 | grep 'ECOLI'
+# aake@ae17a:~/clone30/mfs$ makeblastdb -in uniprot_sprot.fasta -dbtype 'prot'
+
+#aake@ae17a:~/clone30/mfs$ blastall -p blastp -i SV2A.substr -d uniprot_sprot.fasta -m 8 -e 1000 | grep 'ECOLI'
+#1	sp|P0AE24|ARAE_ECOLI	45.161	31	17	0	2	32	199	229	0.056	34.7
+#1	sp|P76230|YDJK_ECOLI	35.294	34	22	0	2	35	203	236	0.46	32.0
+#1	sp|P0AGF4|XYLE_ECOLI	28.571	42	30	0	2	43	221	262	2.3	30.0
+#1	sp|P75916|YCDZ_ECOLI	40.000	35	21	0	51	85	2	36	16	27.3
+#1	sp|P0AEP1|GALP_ECOLI	29.545	44	31	0	2	45	192	235	42	26.6
+#1	sp|P21889|SYD_ECOLI	28.814	59	37	2	2	55	191	249	129	25.0
+
+#aake@ae17a:~/clone30/mfs$ blastall -p blastp -i SV2A.substr -d uniprot_sprot.fasta -m 8 -e 1000 | grep 'ECOLI'
+#1	sp|P32704|YJCF_ECOLI	18.421	76	62	0	36	111	174	249	136	26.2
+#1	sp|P77148|YDHS_ECOLI	29.730	37	24	1	44	78	103	139	159	25.8
+#1	sp|P75948|THIK_ECOLI	36.111	36	22	1	1	35	202	237	301	25.0
+#1	sp|P15723|DGTP_ECOLI	30.909	55	32	2	7	61	45	93	432	24.6
+#1	sp|P45804|YHGE_ECOLI	60.000	15	6	0	85	99	172	186	517	24.3
+
+#aake@ae17a:~/clone30/mfs$ grep 'YDJK' ecoli.txt 
+#b1775; JW5290                      YDJK_ECOLI   P76230     EG13487                459  ydjK
+#aake@ae17a:~/clone30/mfs$ grep 'b1775' interactions.txt 
+#ydjH__b1772	ydjK__b1775
+#entF__b0586	ydjK__b1775
+#abgB__b1337	ydjK__b1775
+#ribE__b0415	ydjK__b1775
+
+# issues in old version
+# last folder not uc name
+# rename MAKE_report
+# GLOBAL_run not prepared for releast
+
+# command line modeller for salt bridges/
+# DE/KR in the topcons
+# make guide which are close
+# check conservation in some homologs
+#####################################################################
+
+
 my @input;
 
 my $ps = `ps | wc -l `;
@@ -57,7 +100,7 @@ goto die1;
 ` mkdir LOOP `;
 
 my @python = split ( ' ', ` python topcons2_wsdl.py -m submit -seq seq.txt ` );
-
+` rm -f seq.txt `;
 
 print GREEN, (split(/\|/, $GN2[0]))[2], ' ';
 print BLUE $python[11], RESET "\n";
@@ -90,12 +133,12 @@ $TOPCONS = 'M'.$TOPCONS.'M';
 
 #print $TOPCONS, "\n";
 
-my @matches = $TOPCONS =~ /(Mo{30,}M|Mi{30,}M)/g;
+my @matches = $TOPCONS =~ /(Mo{$LOOP_LENGTH,}M|Mi{$LOOP_LENGTH,}M)/g;
 
 
 #print RED, "@matches\n", RESET;
 
-open(FILE, ">LOOP/substr.txt");
+open(FILE, ">LOOP/$input[1].substr");
 
 for(my $i=0; $i<@matches; $i++) {
 #print index($TOPCONS, $matches[$i]) , "\t", index($TOPCONS, $matches[$i]) + length($matches[$i]), "\n" ;
@@ -117,7 +160,9 @@ $TOPCONS =~ s/^.//; # chop the first character
 chop $TOPCONS;
 print GREEN, $TOPCONS, RESET, "\n";
 
+` rm -f seq.txt `;
 
+system (" ./step20.pl ");
 
 exit;
 
---
+++ step20.pl
@@ -0,0 +1,92 @@
+#! /usr/bin/perl -w 
+use strict;
+use Data::Dumper;
+ use Term::ANSIColor qw(:constants);
+
+` rm -f EC_MP_PPI_unfiltered_raw_Apr11-2017.txt `;
+` rm -f ecoli.txt `;
+
+print RED, "Downloading .";
+` wget -nc -q "https://www.uniprot.org/docs/ecoli.txt" `;
+print RED, '.';
+` wget -nc -q "http://ecoli.med.utoronto.ca/membrane/php/data/Babu_et_al_2017_Ecoli_CEP_PPI_APMS_data.zip" `;
+print RED, '.';
+print RESET, "\n";
+
+` unzip Babu_et_al_2017_Ecoli_CEP_PPI_APMS_data.zip `;
+` rm -f README `;
+` rm -f Babu_et_al_2017_Ecoli_CEP_PPI_APMS_data.zip `;
+` rm -f EC_MP_WT_PPI_unfiltered_raw_Apr11-2017.txt `;
+
+` rm -f *.phr `;
+` rm -f *.pin `;
+` rm -f *.psq `;
+
+if( -e "uniprot_sprot.fasta" ) {
+print RED, "Building DB ...";
+` makeblastdb -in uniprot_sprot.fasta -dbtype 'prot' `;
+} else { print 'uniprot_sprot.fasta missing...', "\n"; exit; }
+print RESET, "\n";
+
+my $target = ` ls LOOP/*.substr `;
+chomp $target;
+print GREEN, '_'.$target.'_', RESET, "\n";
+
+my $wc = ` cat $target | wc -l `;
+chomp $wc;
+#print $wc, "\n";
+for(my $j=1; $j<$wc+1; $j++) {
+` head -n $j $target | tail -n 1 > temp.seq `;
+
+my @hits = ` blastall -p blastp -i temp.seq -d uniprot_sprot.fasta -m 8 -e 1000 | grep 'ECOLI' `;
+#print BLUE, "@hits\n", RESET;
+
+print "@@@@@@@@@@@@@@@@@@@@@@\n LOOP $j \n@@@@@@@@@@@@@@@@@@@@@@\n";
+
+for(my $i=0; $i<@hits; $i++) {
+my $id  = (split(/\s|\|/, $hits[$i]))[3];
+print $id, "\t";
+
+my $b_number = (split( /\s|;/, ` grep '$id' ecoli.txt ` ))[0];
+print $b_number, "\n";
+
+my @interactants = ` grep '$b_number' EC_MP_PPI_unfiltered_raw_Apr11-2017.txt | head -n 1 `;
+
+my %hash;
+%hash=();
+undef %hash;
+
+for(my $k=0; $k<@interactants; $k++) {
+my $interactant_1 =  (split( /\s|_/, $interactants[$k] ))[4] ;
+my $interactant_2 =  (split( /\s|_/, $interactants[$k] ))[7] ;
+
+if ( $interactant_1 ne $b_number ) { $hash{$interactant_1}++; }
+if ( $interactant_2 ne $b_number ) { $hash{$interactant_2}++; }
+
+#print GREEN, '_'.$interactant_1.'_', RESET, "-";
+#print GREEN, '_'.$interactant_2.'_', RESET, "\n";
+
+}
+
+print Dumper(%hash), "\n";
+
+}
+
+}
+
+` rm -f ecoli.txt `;
+` rm -f EC_MP_PPI_unfiltered_raw_Apr11-2017.txt `;
+` rm -f temp.seq `;
+` rm -f *.phr `;
+` rm -f *.pin `;
+` rm -f *.psq `;
+
+# http://ecoli.med.utoronto.ca/membrane/php/home.php
+# https://www.uniprot.org/docs/ecoli.txt
+# http://ecoli.med.utoronto.ca/membrane/php/data/Babu_et_al_2017_Ecoli_CEP_PPI_APMS_data.zip
+# aake@ae17a:~/clone30/mfs$ blastall -p blastp -i SV2A.substr -d uniprot_sprot.fasta -m 8 -e 1000 | grep 'ECOLI'
+# aake@ae17a:~/clone30/mfs$ makeblastdb -in uniprot_sprot.fasta -dbtype 'prot'
+
+
+exit;
+
